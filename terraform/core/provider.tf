b09460864578d938a547f67c47f27c8bb3c5b12a
Minh Nguyen <22520871@gm.uit.edu.vn>
2025-12-02 23:42:00 +0700

    Add RG

--- terraform/core/provider.tf
+++ terraform/core/provider.tf
@@ -10,6 +10,8 @@ terraform {
 provider "azurerm" {
   features {}
 
-  use_oidc = true
+  use_oidc        = true
+  subscription_id = "b8c602f1-c47f-45f3-bfc3-3ac4c0072bbf"
+  tenant_id       = "7dc2e42d-5de4-4e86-bca3-c5ed9ad61d56"
 }
 
