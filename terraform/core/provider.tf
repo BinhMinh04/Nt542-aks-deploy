75c535652c583d8037ff392f6fd1b67a6c435962
Minh Nguyen <22520871@gm.uit.edu.vn>
2025-12-04 00:50:14 +0700

    Config helm and kyverno

--- terraform/core/provider.tf
+++ terraform/core/provider.tf
@@ -30,11 +30,18 @@ provider "azurerm" {
   tenant_id       = var.tenant_id
 }
 
-provider "helm" {}
+provider "helm" {
+  kubernetes {
+    host                   = try(azurerm_kubernetes_cluster.aks.kube_config.0.host, "")
+    client_certificate     = try(base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate), "")
+    client_key             = try(base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key), "")
+    cluster_ca_certificate = try(base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate), "")
+  }
+}
 
 provider "kubernetes" {
-  host                   = azurerm_kubernetes_cluster.aks.kube_config.0.host
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
+  host                   = try(azurerm_kubernetes_cluster.aks.kube_config.0.host, "")
+  client_certificate     = try(base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate), "")
+  client_key             = try(base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key), "")
+  cluster_ca_certificate = try(base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate), "")
 }
