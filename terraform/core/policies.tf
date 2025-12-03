9af0e1afa9da56de9c9d6eab12acc3c91938ca13
Minh Nguyen <22520871@gm.uit.edu.vn>
2025-12-04 00:54:30 +0700

    Config helm and kyverno

--- terraform/core/policies.tf
+++ terraform/core/policies.tf
@@ -1,29 +1,46 @@
-# CIS 4.2.1: Disallow privileged containers
-resource "kubernetes_manifest" "disallow_privileged" {
-  manifest = yamldecode(file("${path.module}/../polices/disallow-privileged.yaml"))
+# # CIS 4.2.1: Disallow privileged containers
+# resource "kubernetes_manifest" "disallow_privileged" {
+#   manifest = yamldecode(file("${path.module}/../polices/disallow-privileged.yaml"))
   
-  depends_on = [
-    helm_release.kyverno,
-    azurerm_kubernetes_cluster.aks
-  ]
-}
+#   depends_on = [
+#     helm_release.kyverno,
+#     azurerm_kubernetes_cluster.aks
+#   ]
+# }
 
-# CIS 4.2.2: Disallow host PID
-resource "kubernetes_manifest" "disallow_host_pid" {
-  manifest = yamldecode(file("${path.module}/../polices/disallow-host-pid.yaml"))
+# # CIS 4.2.2: Disallow host PID
+# resource "kubernetes_manifest" "disallow_host_pid" {
+#   manifest = yamldecode(file("${path.module}/../polices/disallow-host-pid.yaml"))
   
-  depends_on = [
-    helm_release.kyverno,
-    azurerm_kubernetes_cluster.aks
-  ]
-}
+#   depends_on = [
+#     helm_release.kyverno,
+#     azurerm_kubernetes_cluster.aks
+#   ]
+# }
 
-# CIS 4.6.3: Disallow default namespace
-resource "kubernetes_manifest" "disallow_default_ns" {
-  manifest = yamldecode(file("${path.module}/../polices/disallow-default-namespace.yaml"))
+# # CIS 4.6.3: Disallow default namespace
+# resource "kubernetes_manifest" "disallow_default_ns" {
+#   manifest = yamldecode(file("${path.module}/../polices/disallow-default-namespace.yaml"))
   
+#   depends_on = [
+#     helm_release.kyverno,
+#     azurerm_kubernetes_cluster.aks
+#   ]
+# }
+
+resource "null_resource" "apply_policies" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      az aks get-credentials --resource-group ${azurerm_resource_group.main.name} --name ${azurerm_kubernetes_cluster.aks.name} --overwrite-existing
+      kubectl apply -f ${path.module}/../polices/disallow-privileged.yaml
+      kubectl apply -f ${path.module}/../polices/disallow-host-pid.yaml
+      kubectl apply -f ${path.module}/../polices/disallow-default-namespace.yaml
+    EOT
+  }
+
   depends_on = [
-    helm_release.kyverno,
-    azurerm_kubernetes_cluster.aks
+    kubernetes_manifest.disallow_privileged,
+    kubernetes_manifest.disallow_host_pid,
+    kubernetes_manifest.disallow_default_ns
   ]
 }
