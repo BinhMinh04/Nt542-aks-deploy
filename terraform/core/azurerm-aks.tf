2c04f12d5994c0303a6d81d7d3ea6f3b3e894257
Minh Nguyen <22520871@gm.uit.edu.vn>
2025-12-04 00:38:19 +0700

    Config helm and kyverno

--- terraform/core/azurerm-aks.tf
+++ terraform/core/azurerm-aks.tf
@@ -1,7 +1,7 @@
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "aks-${var.environment}-${var.group}-${var.short_location}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
   dns_prefix          = "aks-${var.environment}-${var.group}-${var.short_location}-dns"
 
   # CIS 5.4.2: Private Cluster
