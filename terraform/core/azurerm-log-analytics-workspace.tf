0efc584d7420fa70906d441556c0acf0e18d42ba
Minh Nguyen <22520871@gm.uit.edu.vn>
2025-12-03 08:12:47 +0700

    Add depend on for logs

--- terraform/core/azurerm-log-analytics-workspace.tf
+++ terraform/core/azurerm-log-analytics-workspace.tf
@@ -1,9 +1,7 @@
 resource "azurerm_log_analytics_workspace" "law" {
-  name                = "law-${azurerm_kubernetes_cluster.aks.name}"
+  name                = "law-${var.environment}-${var.group}-${var.short_location}"
   location            = var.location
   resource_group_name = azurerm_resource_group.main.name
   sku                 = "PerGB2018"
   retention_in_days   = 30
-
-  depends_on = [azurerm_kubernetes_cluster.aks]
 }
