98aac53c6392206a69af07d1d6b70e334c794181
Minh Nguyen <22520871@gm.uit.edu.vn>
2025-12-03 08:15:02 +0700

    Add depend on for logs

--- terraform/core/azurerm-policy.tf
+++ terraform/core/azurerm-policy.tf
@@ -2,11 +2,10 @@ data "azurerm_policy_set_definition" "cis" {
   display_name = "Azure Kubernetes Service Baseline Policies"
 }
 
-resource "azurerm_policy_assignment" "cis_assign" {
+resource "azurerm_resource_policy_assignment" "cis_assign" {
   name                     = "assign-cis-aks"
-  scope                    = azurerm_kubernetes_cluster.aks.id
-  policy_set_definition_id = data.azurerm_policy_set_definition.cis.id
-  location                 = var.location
+  resource_id              = azurerm_kubernetes_cluster.aks.id
+  policy_definition_id     = data.azurerm_policy_set_definition.cis.id
 
   identity {
     type = "SystemAssigned"
